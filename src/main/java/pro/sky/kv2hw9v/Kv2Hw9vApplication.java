058c3de5fccb24e3d358c497f16b8d7c442f4ef7
Prudnikova <dakoto4ka@yandex.ru>
2023-05-19 15:27:04 +0300

    Initial commit

---
+++ src/main/java/pro/sky/kv2hw9v/Kv2Hw9vApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.kv2hw9v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kv2Hw9vApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kv2Hw9vApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/kv2hw9v/Kv2Hw9vApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.kv2hw9v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kv2Hw9vApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
